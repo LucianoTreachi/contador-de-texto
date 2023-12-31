e376a274fa661b4af79d5c5a7d5c171060120799
lucianotreachi <lucianotreachi@outlook.com>
2023-12-31 10:46:47 -0300

    Inicializar Proyecto

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+  <img src="https://raw.githubusercontent.com/LucianoTreachi/Text-Counter/main/assets/readme-banner.jpg" width="100%" alt="Banner"/>
+</div>
+
+### ✅ Proyecto
+
+- Desarrollé una herramienta online que permite obtener rápidamente el número de caracteres, palabras, oraciones y
+  párrafos de un texto.
+
+### ⚡ Tecnologías Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+
+### 👽 Autor
+
+- Luciano Treachi
---
+++ js/script.js
@@ -0,0 +1,55 @@
+const textarea = document.querySelector(".textarea")
+const numberOfCharacters = document.querySelector(".number-of-characters")
+const numberOfWords = document.querySelector(".number-of-words");
+const numberOfRepeatWords = document.querySelector(".number-of-repeat-words");
+const numberOfPhrases = document.querySelector(".number-of-phrases")
+const numberOfParagraphs = document.querySelector(".number-of-paragraphs");
+
+const resetButton = document.querySelector(".reset-button");
+const closeIcon = document.querySelector(".close-icon");
+
+// CHARACTERS
+function countCharacters() {
+  const characters = textarea.value.length;
+  numberOfCharacters.textContent = characters;
+}
+
+// WORDS
+function countWords() {
+  const words = textarea.value.split(/\s|[^\wáéíóúüÁÉÍÓÚÜñÑàèìòùÀÈÌÒÙäëïöüÄËÏÖÜ\/\-]/).filter(word => word.length > 0).length;
+  numberOfWords.textContent = words;
+}
+
+// PHRASES
+function countPhrases() {
+  const phrases = textarea.value.split(/[\.\!\?]+(\s+|$)/).filter(phrase => phrase.trim().length > 0).length;
+  numberOfPhrases.textContent = phrases;
+}
+
+// PARAGRAPHS
+function countParagraphs() {
+  const paragraphs = textarea.value.split(/\n\s*/).filter(paragraph => paragraph.trim().length > 0).length;
+  numberOfParagraphs.textContent = paragraphs;
+}
+
+// UPDATE
+function updateCounts() {
+  countCharacters();
+  countWords();
+  countPhrases();
+  countParagraphs();
+}
+
+// RESET
+function resetCounts() {
+  textarea.value = "";
+  numberOfCharacters.textContent = 0;
+  numberOfWords.textContent = 0;
+  numberOfPhrases.textContent = 0;
+  numberOfParagraphs.textContent = 0;
+}
+
+// LISTENERS
+textarea.addEventListener("input", updateCounts);
+resetButton.addEventListener("click", resetCounts);
+closeIcon.addEventListener("click", resetCounts);
